2fbbc3dbb8cb40c5fde737b83f6def65d2d8eb59
Chen Zhiqiang <1921437884@qq.com>
2022-12-27 16:47:39 +0800

    feat: 添加登录页面

---
+++ routes/sign-in.tsx
@@ -0,0 +1,123 @@
+import { useState } from "preact/hooks";
+import { Head } from "$fresh/runtime.ts";
+import { Handlers } from "$fresh/server.ts";
+
+interface User {
+  username: string;
+  password: string;
+}
+
+export const handler: Handlers<User> = {
+  POST(req, ctx) {
+    const url = new URL(req.url);
+    const username = url.searchParams.get('username') || '';
+    const password = url.searchParams.get('password') || '';
+    
+    return ctx.render({ username, password });
+  },
+};
+
+export default function SignIn() {
+  const [username, setUname] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleValue = (e: any) => {
+    console.log(username, password);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-sm space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Or{' '}
+              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                start your 14-day free trial
+              </a>
+            </p>
+          </div>
+
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="username" className="sr-only">
+                  用户名
+                </label>
+                <input
+                  id="username"
+                  name="username"
+                  type="username"
+                  autoComplete="username"
+                  required
+                  className="relative block w-full appearance-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="用户名"
+                  value={username}
+                  onInput={(e) => setUname(e.currentTarget.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  用户密码
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="用户密码"
+                  value={password}
+                  onInput={(e) => setPassword(e.currentTarget.value)}
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  checked={true}
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
+                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                  记住我
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                  忘记密码?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                登录
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
