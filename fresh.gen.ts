483ec515e48dbb07107bcbf99f89900f74604e13
xuchao <879642177@qq.com>
2022-12-27 17:11:26 +0800

    feat: 新增查询设备影子数据api，支持签名

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,15 +3,19 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/api/joke.ts";
-import * as $1 from "./routes/index.tsx";
+import * as $0 from "./routes/api/iot/[project]/devices/[device]/properties.tsx";
+import * as $1 from "./routes/api/iot/tools/signer.ts";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/SignIn.tsx";
 
 const manifest = {
   routes: {
-    "./routes/api/joke.ts": $0,
-    "./routes/index.tsx": $1,
+    "./routes/api/iot/[project]/devices/[device]/properties.tsx": $0,
+    "./routes/api/iot/tools/signer.ts": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/api/iot/[project]/devices/[device]/properties.tsx
@@ -0,0 +1,45 @@
+// deno-lint-ignore-file
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { Signer } from '../../../tools/signer.ts';
+
+//Set the AK/SK to sign and authenticate the request.
+const Key = "TM2ZMQVGYNMVAVPU2QLN";
+const Secret = "0AbAjSJznraA0MsZUezX0hoMcrV5hOHrZEgzzKDH";
+
+export const handler: Handlers<null> = {
+  async GET(_, ctx) {
+    const { project, device } = ctx.params;
+    console.log(ctx.params);
+    const url =
+      `https://iotda.cn-north-4.myhuaweicloud.com/v5/iot/${project}/devices/${device}/shadow`;
+
+    const request = new Request(url, {
+      method: "GET",
+    });
+
+    const signer = new Signer(Key, Secret);
+    await signer.sign(request);
+
+    const response = await fetch(request);
+    if (response.status === 404) {
+      return ctx.render(null);
+    }
+
+    const data = (await response.json()).shadow[0].reported.properties;
+    console.log(data);
+    return ctx.render(data);
+  },
+};
+
+export default function Page({ data }: PageProps<any | null>) {
+  if (!data) {
+    return <h1>Properties not found</h1>;
+  }
+
+  return (
+    <div>
+      <h1>温度: {data.Temperature}</h1>
+      <p>湿度：{data.Humidity}</p>
+    </div>
+  );
+}
