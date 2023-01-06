c89dd2a66d084599e837d4f1a7b98bca7d5866c9
Chen Zhiqiang <1921437884@qq.com>
2023-01-06 16:04:31 +0800

    feat: 增加 shell 启动脚本

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -e '.bin/deno' ] then
+  echo 'Deno not found. Installing...'
+  unzip .bin/deno-x86_64-unknown-linux-gnu.zip -d .bin
+  echo 'Deno installed in .bin/deno'
+fi
+
+echo 'Starting Deno...'
+
+'.bin/deno' task start
