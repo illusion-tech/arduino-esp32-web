5cc780b5b2fcc6477c7c32ad11789a780965d02e
Chen Zhiqiang <1921437884@qq.com>
2023-01-09 16:11:38 +0800

    fix: shell 启动脚本语法错误

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -e '.bin/deno' ] then
+if [ ! -e '.bin/deno' ]; then
   echo 'Deno not found. Installing...'
   unzip .bin/deno-x86_64-unknown-linux-gnu.zip -d .bin
   echo 'Deno installed in .bin/deno'
